aa62e208bb4972b73b6fc3fa53247e4e64372a0b
engkernel <engkernel@gmail.com>
2024-09-12 18:53:27 +0000

    ext2 structures done

--- src/fs/ext2/ext2.h
+++ src/fs/ext2/ext2.h
@@ -154,13 +154,72 @@ struct ext2_block_group_desc
 
 
 // inode
-// defined reserved indoes
+// defined reserved inodes
 #define EXT2_BAD_INO		1
 #define EXT2_ROOT_INO		2
 #define EXT2_ACL_IDX_INO	3
 #define EXT2_ACL_DATA_INO	4
 #define EXT2_BOOT_LOADER_INO	5
 #define EXT2_UNDEL_DIR_INO	6
+
+// defined i_mode values
+#define EXT2_S_IFSOCK		0xC000
+#define EXT2_S_IFLNK		0xA000
+#define EXT2_S_IFREG		0x8000
+#define EXT2_S_IFBLK		0x6000
+#define EXT2_S_IFDIR		0x4000
+#define EXT2_S_IFCHR		0x2000
+#define EXT2_S_IFIFO		0x1000
+
+// process execution user/group override
+#define EXT2_S_ISUID		0x0800
+#define EXT2_S_ISGID		0x0400
+#define EXT2_S_ISVTX		0x0200
+
+// access rights
+#define EXT2_S_IRUSR		0x0100
+#define EXT2_S_IWUSR		0x0080
+#define EXT2_S_IXUSR		0x0040
+#define EXT2_S_IRGRP		0x0020
+#define EXT2_S_IWGRP		0x0010
+#define EXT2_S_IXGRP		0x0008
+#define EXT2_S_IROTH		0x0004
+#define EXT2_S_IWOTH		0x0002
+#define EXT2_S_IXOTH		0x0001
+
+// defined i_flags values
+#define EXT2_SECRM_FL		0x00000001
+#define EXT2_UNRM_FL		0x00000002
+#define EXT2_COMPR_FL		0x00000004
+#define EXT2_SYNC_FL		0x00000008
+#define EXT2_IMMUTABLE_FL	0x00000010
+#define EXT2_APPEND_FL		0x00000020
+#define EXT2_NODUMP_FL		0x00000040
+#define EXT2_NOATIME_FL		0x00000080
+
+// reserved for compression usage
+#define EXT2_DIRTY_FL		0x00000100
+#define EXT2_COMPRBLK_FL	0x00000200
+#define EXT2_NOCOMPR_FL		0x00000400
+#define EXT2_ECOMPR_FL		0x00000800
+
+// end of compression flags
+#define EXT2_BTREE_FL		0x00001000
+#define EXT2_INDEX_FL		0x00001000
+#define EXT2_IMAGIC_FL		0x00002000
+#define EXT3_JOURNAL_DATA_FL	0x00004000
+#define EXT2_RESERVED_FL	0x80000000
+
+// Linux
+struct i_osd2
+{
+	uint8_t		l_i_frag;
+	uint8_t		l_i_fsize;
+	uint16_t	reserved;
+	uint16_t	l_i_uid_high;
+	uint16_t	l_i_gid_high;
+	uint32_t	reserved2;
+};
 
 struct ext2_inode
 {
@@ -182,6 +241,26 @@ struct ext2_inode
 	uint32_t	i_dir_acl;
 	uint32_t	i_faddr;
 	uint32_t	i_osd2;
+};
+
+// defined inode file type values
+#define EXT2_FT_UNKNOWN		0
+#define EXT2_FT_REG_FILE	1
+#define	EXT2_FT_DIR		2
+#define EXT2_FT_CHRDEV		3
+#define EXT2_FT_BLKDEV		4
+#define EXT2_FT_FIFO		5
+#define	EXT2_FT_SOCK		6
+#define EXT2_FT_SYMLINK		7
+
+// directory entry structure
+struct ext2_dir_entry
+{
+	uint32_t	inode;
+	uint16_t	rec_len;
+	uint8_t		name_len;
+	uint8_t		file_type;
+	unsigned char	name[255];
 };
 
 #endif
